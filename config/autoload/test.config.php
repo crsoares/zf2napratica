a0e0ef916bc3014629d2a77aab7437a51e4f0b07
crysthianophp <crysthianophp@gmail.com>
2015-03-11 23:05:06 -0300

    estudos zf2

--- config/autoload/test.config.php
+++ config/autoload/test.config.php
@@ -2,12 +2,12 @@
 
 return array(
 	'db' => array(
-		'driver' => 'Pdo',
+		'driver' => 'PDO',
 		'dsn' => 'mysql:dbname=zf2napratica_test;host=localhost',
 		'username' => 'root',
 		'password' => '',
 		'driver_options' => array(
-			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMAES \'UTF8\'',
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'',
 		),
 	),
 );
